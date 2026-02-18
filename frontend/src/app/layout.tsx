e285695ce490dd5d0b6a06fa57b3c462f09c8ec4
realhesam <e.hesamkasaeian@gmail.com>
2026-02-18 22:47:22 +0330

    up files5

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+//test
 import LayoutCondition from "@/layout/LayoutCondition";
 import { NotificationProvider } from "@/layout/NotificationProvider";
 import type { Metadata } from "next";
@@ -5,32 +6,26 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const rubikFont = localFont({
-  src: [{ path: "../../public/fonts/Rubik/Rubik-variable.ttf" }],
-  variable: "--font-rubik",
+    src: [{ path: "../../public/fonts/Rubik/Rubik-variable.ttf" }],
+    variable: "--font-rubik",
 });
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | هنرینو",
-    default: "هنرینو | صفحه اصلی",
-  },
-  description: "new Project",
+    title: {
+        template: "%s | هنرینو",
+        default: "هنرینو | صفحه اصلی",
+    },
+    description: "new Project",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="fa" dir="rtl">
-      <body className={`${rubikFont.variable} antialiased bg-stone-100`}>
-        <NotificationProvider>
-          <LayoutCondition>
-            {children}
-          </LayoutCondition>
-        </NotificationProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="fa" dir="rtl">
+            <body className={`${rubikFont.variable} antialiased bg-stone-100`}>
+                <NotificationProvider>
+                    <LayoutCondition>{children}</LayoutCondition>
+                </NotificationProvider>
+            </body>
+        </html>
+    );
 }
