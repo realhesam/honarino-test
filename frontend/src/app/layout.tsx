c294d4c51e62b4c91f6ff2b8cd61017ba3378c5b
realhesam <e.hesamkasaeian@gmail.com>
2026-02-18 22:57:18 +0330

    check ci workflow

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,8 +17,6 @@ export const metadata: Metadata = {
     description: "new Project",
 };
 
-//hello world
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="fa" dir="rtl">
