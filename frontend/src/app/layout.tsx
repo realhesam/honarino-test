867884e711883fe628a8159c317e143440d58a8c
realhesam <e.hesamkasaeian@gmail.com>
2026-02-18 22:52:41 +0330

    up files7

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
     description: "new Project",
 };
 
+//hello world
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="fa" dir="rtl">
